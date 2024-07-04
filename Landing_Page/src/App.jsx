922dc21a0fdc9552c6bb805e7bee5f0d04e116e5
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-07-04 18:08:04 +0530

    initial commit

---
+++ Landing_Page/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
